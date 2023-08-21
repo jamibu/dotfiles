ed5ce657352ea1d30012c67dc7feec57c99df688
jamibu <bucher.codes@gmail.com>
2023-08-21 22:13:48 +1000

    New Neovim config using lazy.nvim for packages

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,26 @@
-require("jamibu")
--- disable netrw at the very start of your init.lua (strongly advised)
+require("core")
+
+-- Important to set before the Lazy stuff
+vim.g.mapleader = " "
+
+-- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+
+-- Plugin Manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup('plugins')
+pcall(vim.cmd.colorscheme, 'catppuccin')
