23d454b7294fe8c00397c98b81a2c120f5a7c83b
jamibu <bucher.codes@gmail.com>
2024-01-28 17:25:00 +1100

    Clean up unused config.

--- nvim_old/nvim1/after/plugin/init.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd(":TSInstall all");
-require("nvim-autopairs").setup {}
-require('neogit').setup {}
-require('lualine').setup {}
-require('leap').add_default_mappings()
-require('colorizer').setup()
-require('mason').setup()
