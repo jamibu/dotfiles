04a5cc1ce3320c1952991f941493e8d05a15a5e1
jamibu <bucher.codes@gmail.com>
2024-02-03 01:28:10 +1100

    Finishing setup for stow

--- nvim/lua/plugins/colours.lua
+++
@@ -1,5 +0,0 @@
-return {
-    -- { 'catppuccin/nvim', as = 'catppuccin', event = "VeryLazy" },
-    { "EdenEast/nightfox.nvim", event = "VeryLazy" },
-    -- { 'folke/tokyonight.nvim',  event = "VeryLazy" },
-}
--- nvim/lua/plugins/lsp-signature.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "ray-x/lsp_signature.nvim",
-  event = "BufReadPost",
-  opts = {},
-  config = function(_, opts) require'lsp_signature'.setup(opts) end
-}
