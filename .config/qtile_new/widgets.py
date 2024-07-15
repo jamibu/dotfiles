9dc66480c4f7fcba44f41ab5bf99370aed5e3a91
Jack Bucher <jack@localhost.localdomain>
2024-07-15 15:22:22 +1000

    Add new Qtile config

---
+++ .config/qtile_new/widgets.py
@@ -0,0 +1,52 @@
+from libqtile import widget
+from libqtile import qtile
+
+widget_defaults = dict(
+    font='JetBrainsMono Nerd Font',
+    fontsize=14,
+    padding=1,
+)
+
+extension_defaults = widget_defaults.copy()
+class MyVolume(widget.Volume):
+    def _configure(self, qtile, bar):
+        widget.Volume._configure(self, qtile, bar)
+        self.volume, self.mute = self.get_volume()
+        if self.mute:
+            self.text = ''
+        else:
+            if self.volume <= 0:
+                self.text = ''
+            elif self.volume <= 33:
+                self.text = ''
+            elif self.volume < 66:
+                self.text = ''
+            else:
+                self.text = ''
+        self.draw()
+        # drawing here crashes Wayland
+
+    def _update_drawer(self, wob=False):
+        if self.mute:
+            self.text = ''
+        else:
+            if self.volume <= 0:
+                self.text = ''
+            elif self.volume <= 33:
+                self.text = ''
+            elif self.volume < 66:
+                self.text = ''
+            else:
+                self.text = ''
+        self.draw()
+
+        if wob:
+            with open(self.wob, 'a') as f:
+                f.write(str(self.volume) + "\n")
+
+volume = MyVolume(
+    fontsize=14,
+    font='JetBrainsMono Nerd Font',
+    foreground="#da8548",
+    mouse_callbacks = {'Button1': lambda: qtile.spawn("pavucontrol")}
+)
