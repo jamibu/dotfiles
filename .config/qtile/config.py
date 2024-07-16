df7ceb0c237bd452fc298673886beaea9c1598d4
Jack Bucher <jack@localhost.localdomain>
2024-07-16 23:44:29 +1000

    Updates for new PC setup

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -23,81 +23,161 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import os
 import subprocess
+import os
 
 from libqtile import layout, hook
-from libqtile.config import Click, Drag, Match
+from libqtile.config import Match, Screen, Group, Key, Drag, Click
 from libqtile.lazy import lazy
 
-from screens import init_screen_main, init_screen, init_screen_vert
-from groups import init_groups, assign_group_keys
-from keys import keys, mod
+from internal.keymaps import add_vt_bindings, add_group_bindings
+from bar import init_bar, init_bar_main
 
-
-groups = init_groups()
-keys = assign_group_keys(keys, mod, groups)
-
-layout_theme = {
-    "border_width": 2,
-    "margin": 7,
-    "border_focus": "e1acff",
-    "border_normal": "1D2330"
-}
-
-layouts = [
-    layout.Max(**layout_theme),
-    layout.MonadTall(**layout_theme),
-    # Try more layouts by unleashing below layouts.
-    # layout.Stack(num_stacks=2),
-    # layout.Matrix(),
-    layout.MonadWide(**layout_theme),
-    # layout.RatioTile(),
-    # layout.Tile(),
-    # layout.TreeTab(),
-    # layout.VerticalTile(),
-    # layout.Zoomy(),
-]
-colors = [
-    ["#282c34", "#282c34"],
-    ["#1c1f24", "#1c1f24"],
-    ["#dfdfdf", "#dfdfdf"],
-    ["#ff6c6b", "#ff6c6b"],
-    ["#98be65", "#98be65"],
-    ["#da8548", "#da8548"],
-    ["#51afef", "#51afef"],
-    ["#c678dd", "#c678dd"],
-    ["#46d9ff", "#46d9ff"],
-    ["#a9a1e1", "#a9a1e1"]
+terminal = "alacritty"
+groups = [
+    Group("1", label="一", layout="max"),
+    Group("2", label="二", layout="monadtall"),
+    Group("3", label="三", layout="monadtall"),
+    Group("4", label="四", layout="monadtall"),
+    Group("5", label="五", layout="monadtall"),
+    Group("6", label="六", layout="monadtall"),
+    Group("7", label="七", layout="monadtall"),
+    Group("8", label="八", layout="monadtall"),
+    Group("9", label="九", layout="monadtall"),
+    Group("0", label="十", layout="max"),
 ]
 
+##############
+# KEYBINDINGS
+##############
+mod = "mod4"
 
-widget_defaults = dict(
-    font="Ubuntu Bold",
-    fontsize=12,
-    padding=2,
-    background=colors[0]
-)
-
-extension_defaults = widget_defaults.copy()
-
-screens = [
-    init_screen_main(),
-    init_screen_vert(),
-    init_screen(),
+keys = [
+    # A list of available commands that can be bound to keys can be found
+    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
+    # Switch focus of monitors
+    Key([mod], "period", lazy.next_screen(), desc='Move focus to next monitor'),
+    Key([mod], "comma", lazy.prev_screen(), desc='Move focus to prev monitor'),
+    # Switch between windows
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    # Move windows between left/right columns or move up/down in current stack.
+    # Moving out of range in Columns layout will create new column.
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+    # Grow windows. If current window is on the edge of screen and direction
+    # will be to screen edge - window would shrink.
+    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key(
+        [mod, "shift"],
+        "Return",
+        lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack",
+    ),
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key(
+        [mod],
+        "f",
+        lazy.window.toggle_fullscreen(),
+        desc="Toggle fullscreen on the focused window",
+    ),
+    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle floating on the focused window"),
+    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod], "r", lazy.spawn("rofi -show drun"), desc="Spawn a command using a prompt widget"),
+    Key([], "XF86AudioRaiseVolume",lazy.spawn("amixer set Master 3%+")),
+    Key([], "XF86AudioLowerVolume",lazy.spawn("amixer set Master 3%-")),
+    Key([], "XF86AudioMute",lazy.spawn("amixer set Master toggle")),
 ]
 
-# Drag floating layouts.
+
+keys = add_vt_bindings(keys)
+keys = add_group_bindings(keys, groups, mod)
+
 mouse = [
     Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
     Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
+##########
+# LAYOUTS
+##########
+layout_theme = {
+    "border_width": 2,
+    "margin": 7,
+    "border_focus": "e1acff",
+    "border_normal": "1D2330"
+}
+layouts = [
+    # layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    layout.Max(**layout_theme),
+    layout.Bsp(**layout_theme),
+    layout.MonadTall(**layout_theme),
+    # layout.MonadWide(),
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Matrix(),
+    # layout.RatioTile(),
+    # layout.Tile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+]
+
+widget_defaults = dict(
+    font='JetBrainsMono Nerd Font Bold',
+    fontsize=12,
+    padding=1,
+    background="#11111B"
+)
+
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        top=init_bar_main(),
+        wallpaper="~/Pictures/aesthetic-wallpapers/images/art-lake.png",
+        wallpaper_mode="stretch",
+        # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
+        # By default we handle these events delayed to already improve performance, however your system might still be struggling
+        # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
+        # x11_drag_polling_rate = 60,
+    ),
+    Screen(
+        top=init_bar(),
+        wallpaper="~/Pictures/aesthetic-wallpapers/images/art-lake.png",
+        wallpaper_mode="stretch",
+        # You can uncomment this variable if you see that on X11 floating resize/moving is laggy
+        # By default we handle these events delayed to already improve performance, however your system might still be struggling
+        # This variable is set to None (no cap) by default, but you can set it to 60 to indicate that you limit it to 60 events per second
+        # x11_drag_polling_rate = 60,
+    ),
+]
+
+# Drag floating layouts.
+
 dgroups_key_binder = None
-dgroups_app_rules = []  # type: list
+dgroups_app_rules = []
 follow_mouse_focus = True
 bring_front_click = False
+floats_kept_above = True
 cursor_warp = False
 floating_layout = layout.Floating(
     float_rules=[
@@ -122,7 +202,7 @@ auto_minimize = True
 @hook.subscribe.startup_once
 def start_once():
     home = os.path.expanduser("~")
-    subprocess.call([home + '/.config/qtile/autostart.sh'])
+    subprocess.call([home + '/.config/qtile/autostart_once.sh'])
 
 # When using the Wayland backend, this can be used to configure input devices.
 wl_input_rules = None
--- .config/qtile/internal/widgets.py
+++ .config/qtile/internal/widgets.py
@@ -13,31 +13,31 @@ class MyVolume(widget.Volume):
         widget.Volume._configure(self, qtile, bar)
         self.volume, self.mute = self.get_volume()
         if self.mute:
-            self.text = ''
+            self.text = ' '
         else:
             if self.volume <= 0:
-                self.text = ''
+                self.text = ' '
             elif self.volume <= 33:
-                self.text = ''
+                self.text = ' '
             elif self.volume < 66:
-                self.text = ''
+                self.text = ' '
             else:
-                self.text = ''
+                self.text = ' '
         self.draw()
         # drawing here crashes Wayland
 
     def _update_drawer(self, wob=False):
         if self.mute:
-            self.text = ''
+            self.text = ' '
         else:
             if self.volume <= 0:
-                self.text = ''
+                self.text = ' '
             elif self.volume <= 33:
-                self.text = ''
+                self.text = ' '
             elif self.volume < 66:
-                self.text = ''
+                self.text = ' '
             else:
-                self.text = ''
+                self.text = ' '
         self.draw()
 
         if wob:
