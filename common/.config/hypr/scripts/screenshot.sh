7411d56fca223c2e4e622f1b9f7a071836fa459b
Jack Bucher <jack.bucher@orygen.org.au>
2025-07-10 20:20:50 +1000

    Improvements to screenshot. Copy to clipboard

---
+++ common/.config/hypr/scripts/screenshot.sh
@@ -0,0 +1,19 @@
+screenshotFolder="$HOME/Pictures/screenshots"
+
+case $1 in
+    'active')
+        activeWindow=$(hyprctl -j activewindow | jq -r '(.class)')
+        filename="$(date +'%Y%m%d%H%M%S')-$activeWindow.png"
+        hyprctl -j activewindow | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"' \
+            | grim -g - - | tee "$screenshotFolder/$filename" | wl-copy
+        ;;
+
+    'region')
+        filename=$(date +'%Y%m%d%H%M%S-region.png')
+        slurp | grim -g - - | tee "$screenshotFolder/$filename" | wl-copy
+        ;;
+esac
+
+if [ -e $screenshotFolder/$filename ]; then
+    notify-send -i "$screenshotFolder/$filename" "Screenshot Saved" "$filename"
+fi
