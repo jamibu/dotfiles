1dab5e948fafeb87f4f60dbea117808a99293db2
jamibu <bucher.codes@gmail.com>
2025-07-26 23:50:00 +1000

    Various changes from CachyOS install.

--- personal/.config/fish/config.fish
+++ personal/.config/fish/config.fish
@@ -3,8 +3,6 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 set -gx EDITOR nvim
 set -gx NVIM_APPNAME "kickstart.nvim"
 
-eval $(keychain --eval --quiet id_ed25519)
-
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
