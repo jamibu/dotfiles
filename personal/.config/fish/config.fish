dcbdb21fb69e6b56d09ecc586f0c8c979bda87c7
jamibu <bucher.codes@gmail.com>
2025-07-26 16:31:00 +1000

    Add keychain for fish.

--- personal/.config/fish/config.fish
+++ personal/.config/fish/config.fish
@@ -3,6 +3,7 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 set -gx EDITOR nvim
 set -gx NVIM_APPNAME "kickstart.nvim"
 
+eval $(keychain --eval --quiet id_ed25519)
 
 # overwrite greeting
 # potentially disabling fastfetch
