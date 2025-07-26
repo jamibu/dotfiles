3b1d350fcaa219184966ccc032f85bc18bc9233b
jamibu <bucher.codes@gmail.com>
2025-07-26 16:10:35 +1000

    New dotfiles for personal (CachyOS)

---
+++ personal/.config/fish/config.fish
@@ -0,0 +1,11 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+set -gx EDITOR nvim
+set -gx NVIM_APPNAME "kickstart.nvim"
+
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
