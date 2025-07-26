ac417d8e9289e42096ee7fbda405ee5015324a4e
jamibu <bucher.codes@gmail.com>
2025-07-26 23:50:00 +1000

    Various changes from CachyOS install.

--- common/.config/hypr/scripts/random_wallpaper.sh
+++ common/.config/hypr/scripts/random_wallpaper.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-WALLPAPER_DIR="$HOME/Pictures/wallpaper/"
+WALLPAPER_LIST="$HOME/.config/hypr/scripts/wallpaper"
 CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
 # Get a random wallpaper that is not the current one
-WALLPAPER=$(find "$WALLPAPER_DIR" -type l,f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+WALLPAPER=$(cat "$WALLPAPER_LIST" | grep -wv "$CURRENT_WALL" | shuf -n 1)
 
 # Apply the selected wallpaper
 hyprctl hyprpaper reload ,"$WALLPAPER"
