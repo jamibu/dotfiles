dea4a524274bb968cabf345f91d97733e4fef411
jamibu <bucher.codes@gmail.com>
2025-07-27 01:59:13 +1000

    Small tweaks.

--- personal/.config/fish/config.fish
+++ personal/.config/fish/config.fish
@@ -3,6 +3,8 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 set -gx EDITOR nvim
 set -gx NVIM_APPNAME "kickstart.nvim"
 
+eval (ssh-agent -c)
+
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
