5b69e6e41cabfe9794b6000a759bdb1fa4cd2435
jamibu <bucher.codes@gmail.com>
2026-08-14 19:49:12 +1000

    Add new neovim config

---
+++ nvim/plugin/options.lua
@@ -0,0 +1,29 @@
+vim.g.mapleader = ' '
+
+vim.g.maplocalleader = ' '
+
+-- UI
+vim.g.have_nerd_font = false
+
+vim.o.breakindent = true
+vim.o.breakindentopt = 'list:-1'
+vim.o.colorcolumn = '+1'
+vim.o.cursorline = true
+vim.o.linebreak = true
+vim.o.list = true
+vim.o.number = true
+vim.o.signcolumn = 'yes'
+
+vim.o.wrap = false
+
+-- EDITING
+vim.o.autoindent = true
+vim.o.expandtab = true
+vim.o.formatoptions = 'rqnl1j' -- Improve comment editing
+vim.o.ignorecase = true
+vim.o.incsearch = true
+vim.o.infercase = true
+vim.o.undofile = true
+vim.o.smartcase = true
+vim.o.smartindent = true
+
