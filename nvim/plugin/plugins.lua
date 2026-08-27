02619b37294f4d98740f5a17de663602322f58dc
jamibu <bucher.codes@gmail.com>
2026-08-27 20:26:10 +1000

    Add gitsigns for blame line

--- nvim/plugin/plugins.lua
+++ nvim/plugin/plugins.lua
@@ -8,8 +8,21 @@ vim.api.nvim_create_autocmd("PackChanged", {
 
 vim.pack.add({
   "https://github.com/nvim-treesitter/nvim-treesitter",
-  -- "https://github.com/lewis6991/gitsigns.nvim",
-  -- "https://github.com/NeogitOrg/neogit",
+  "https://github.com/lewis6991/gitsigns.nvim",
+  "https://github.com/NeogitOrg/neogit",
 })
 
--- require('gitsigns').setup()
+require('gitsigns').setup({
+  signcolumn = false,   -- let mini.diff own the gutter
+  numhl = false,
+  linehl = false,
+  word_diff = false,
+  signs = { add = {}, change = {}, delete = {}, topdelete = {}, changedelete = {} },
+
+  current_line_blame = true,  -- always-on, updates as cursor moves
+  current_line_blame_opts = {
+    delay = 300,
+    virt_text_pos = 'eol',
+  },
+  current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+})
