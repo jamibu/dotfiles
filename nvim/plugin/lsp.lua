4039a34351a132ff05575da6ce40952ca82c077a
jamibu <bucher.codes@gmail.com>
2026-08-28 12:17:04 +1000

    Add conform

--- nvim/plugin/lsp.lua
+++ nvim/plugin/lsp.lua
@@ -1,17 +1,36 @@
 vim.pack.add({
-  "https://github.com/neovim/nvim-lspconfig",
-  "https://github.com/mason-org/mason.nvim",
+    "https://github.com/neovim/nvim-lspconfig",
+    "https://github.com/mason-org/mason.nvim",
+    'https://github.com/stevearc/conform.nvim',
 })
 
 vim.lsp.enable({
-  "lua_ls",
-  "gopls",
-  "rust_analyzer",
-  "ols",
-  "ruff",
-  "ty",
-  "prettier",
-  "eslint",
+    "lua_ls",
+    "gopls",
+    "rust_analyzer",
+    "ols",
+    "ruff",
+    "ty",
+    "typescript_language_server",
+    "prettier",
+    "eslint",
+})
+
+-- See also:
+-- - `:h Conform`
+-- - `:h conform-options`
+-- - `:h conform-formatters`
+require('conform').setup({
+    default_format_opts = {
+        -- Allow formatting from LSP server if no dedicated formatter is available
+        lsp_format = 'fallback',
+    },
+    format_on_save = {
+        timeout_ms = 500,
+    },
+    formatters_by_ft = {
+        lua = { "stylua" },
+    }
 })
 
 require('mason').setup()
