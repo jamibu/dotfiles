5419162044e65dc4e8dcc475796a3dc85fbbbafe
jamibu <bucher.codes@gmail.com>
2026-08-28 12:25:18 +1000

    Double quotes

--- nvim/plugin/plugins.lua
+++ nvim/plugin/plugins.lua
@@ -11,13 +11,13 @@ vim.pack.add({
     "https://github.com/lewis6991/gitsigns.nvim",
     "https://github.com/NeogitOrg/neogit",
     "https://github.com/ThePrimeagen/refactoring.nvim",
-    'https://github.com/nvim-lua/plenary.nvim',
-    'https://github.com/folke/trouble.nvim',
-    { src = 'https://github.com/ThePrimeagen/harpoon', version = 'harpoon2' },
+    "https://github.com/nvim-lua/plenary.nvim",
+    "https://github.com/folke/trouble.nvim",
+    { src = "https://github.com/ThePrimeagen/harpoon", version = "harpoon2" },
 })
 
 -- Gitsigns
-require('gitsigns').setup({
+require("gitsigns").setup({
     signcolumn = false, -- let mini.diff own the gutter
     numhl = false,
     linehl = false,
@@ -27,20 +27,20 @@ require('gitsigns').setup({
     current_line_blame = false, -- always-on, updates as cursor moves
     current_line_blame_opts = {
         delay = 300,
-        virt_text_pos = 'eol',
+        virt_text_pos = "eol",
     },
-    current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
+    current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
 })
 
 -- Harpoon
-require('harpoon').setup() -- required, wires up autocmds
+require("harpoon").setup() -- required, wires up autocmds
 
 -- Trouble
-require('trouble').setup()
+require("trouble").setup()
 
 -- Colourscheme
 vim.pack.add({
     "https://github.com/rose-pine/neovim",
     -- "https://github.com/folke/tokyonight.nvim",
 })
-vim.cmd.colorscheme('rose-pine')
+vim.cmd.colorscheme("rose-pine")
