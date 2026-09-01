a9785c2b05265b2f817d7e7eaae5ef1683d02548
jamibu <bucher.codes@gmail.com>
2026-09-01 23:47:06 +1000

    Add colours file.

---
+++ nvim/plugin/colours.lua
@@ -0,0 +1,7 @@
+-- Colourscheme
+vim.pack.add({
+  'https://github.com/rose-pine/neovim',
+  -- 'https://github.com/folke/tokyonight.nvim',
+  -- 'https://github.com/catppuccin/nvim',
+})
+vim.cmd.colorscheme('rose-pine')
