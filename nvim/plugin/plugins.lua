4949fb95e456294008a03fd32b4f76ef7a3ac9bc
jamibu <bucher.codes@gmail.com>
2026-09-01 23:48:44 +1000

    Add oil, treesitter-context + cleanup

--- nvim/plugin/plugins.lua
+++ nvim/plugin/plugins.lua
@@ -1,14 +1,7 @@
-vim.api.nvim_create_autocmd('PackChanged', {
-  callback = function(ev)
-    if ev.data.spec.name == 'nvim-treesitter' and ev.data.kind == 'update' then
-      vim.cmd('TSUpdate')
-    end
-  end,
-})
-
 vim.pack.add({
   'https://github.com/nvim-treesitter/nvim-treesitter',
   'https://github.com/nvim-treesitter/nvim-treesitter-textobjects',
+  'https://github.com/nvim-treesitter/nvim-treesitter-context',
   'https://github.com/lewis6991/gitsigns.nvim',
   'https://github.com/NeogitOrg/neogit',
   'https://github.com/ThePrimeagen/refactoring.nvim',
@@ -42,12 +35,43 @@ require('harpoon'):setup() -- required, wires up autocmds
 -- Trouble
 require('trouble').setup()
 
--- Colourscheme
-vim.pack.add({
-  'https://github.com/rose-pine/neovim',
-  -- "https://github.com/folke/tokyonight.nvim",
-})
-vim.cmd.colorscheme('rose-pine')
-
 -- Oil
-require('oil').setup()
+require('oil').setup({
+  delete_to_trash = true,
+})
+
+require('treesitter-context').setup({
+  enable = true,
+  max_lines = 3,
+  trim_scope = 'outer',
+  mode = 'cursor',
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = '*',
+  callback = function(args)
+    local ft = args.match
+    local lang = vim.treesitter.language.get_lang(ft) or ft
+
+    -- Only bother if nvim-treesitter actually knows this language
+    local ts = require('nvim-treesitter')
+    if not vim.tbl_contains(ts.get_available(), lang) then
+      return
+    end
+
+    -- Install if missing (no-op if already installed), then start highlighting
+    if #vim.api.nvim_get_runtime_file('parser/' .. lang .. '.*', false) == 0 then
+      ts.install(lang):wait(30000) -- blocks up to 30s on first open of a new language
+    end
+
+    pcall(vim.treesitter.start, args.buf, lang)
+  end,
+})
+
+vim.api.nvim_create_autocmd('PackChanged', {
+  callback = function(ev)
+    if ev.data.spec.name == 'nvim-treesitter' and ev.data.kind == 'update' then
+      vim.cmd('TSUpdate')
+    end
+  end,
+})
