0bafc98d70ae1cdcc15703ff7328309ec63fcc5d
Jack Bucher <jack.bucher@orygen.org.au>
2026-09-15 17:19:14 +1000

    fixing php indenting

--- nvim/plugin/plugins.lua
+++ nvim/plugin/plugins.lua
@@ -65,13 +65,9 @@ vim.api.nvim_create_autocmd('FileType', {
     end
 
     pcall(vim.treesitter.start, args.buf, lang)
-  end,
-})
 
-vim.api.nvim_create_autocmd('PackChanged', {
-  callback = function(ev)
-    if ev.data.spec.name == 'nvim-treesitter' and ev.data.kind == 'update' then
-      vim.cmd('TSUpdate')
+    if ft == 'php' then
+      vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
     end
   end,
 })
